86b58e183086945b1c898c727ad6e07d0eacf9c3
Ahaitsham <ahtshamsaifi345@gmail.com>
2026-06-16 20:52:24 +0530

    footer salutation of mine target blank modification

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -58,7 +58,7 @@ export default function Footer() {
 
         <div className={styles.bottom}>
           <p>© 2026 Divya Jyoti Hospital. All Rights Reserved.</p>
-          <p>Made by <a className='salutation' target='blank' href="https://ahaitshamkhan.vercel.app" style={{color:"white"}}>Ahaitsham</a></p>
+          <p>Made by <a className='salutation' target='_blank' href="https://ahaitshamkhan.vercel.app" style={{color:"white"}}>Ahaitsham</a></p>
         </div>
       </footer>
 
