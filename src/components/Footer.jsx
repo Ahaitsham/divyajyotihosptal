9e96424727a825d93a501a487a8611df42c5617e
Ahaitsham <ahtshamsaifi345@gmail.com>
2026-08-15 14:23:06 +0530

    a

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -62,7 +62,7 @@ export default function Footer() {
 
         <div className={styles.bottom}>
           <p>© 2026 Divya Jyoti Hospital. All Rights Reserved.</p>
-          {/* <p> Built by <a className='salutation' target='_blank' href="https://ahaitshamkhan.vercel.app" style={{color:'white'}} ><b>Ahaitsham</b></a></p> */}
+          <p> Built by <a className='salutation' target='_blank' href="https://ahaitshamkhan.vercel.app" style={{color:'white'}} ><b>Ahaitsham</b></a></p>
           {/* <p><a className='salutation' target='_blank' href="https://ahaitshamkhan.vercel.app">© 2026 Divya Jyoti Hospital. All Rights Reserved.</a></p> */}
         </div>
       </footer>
